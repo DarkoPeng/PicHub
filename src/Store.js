5065df2eec5d0765b600987dcaf41a3449aea755
Darko <354492936@qq.com>
2018-09-29 23:08:19 +0800

    feat update 上传组件

--- src/Store.js
+++ src/Store.js
@@ -1,2 +1,1 @@
 import { createStore, applyMiddleware } from 'redux';
-
--- src/components/upload/views/upload.js
+++ src/components/upload/views/upload.js
@@ -4,13 +4,36 @@ import "./upload.css";
 class Upload extends Component {
   constructor() {
     super(...arguments);
+    this.state = {
+      linkUrl: ""
+    };
+
+    this.fileInput = React.createRef();
+    this.dropFile = this.dropFile.bind(this);
+    this.dragOverFile = this.dragOverFile.bind(this);
+  }
+
+  // 从本地文件夹拖拽上传
+  dropFile(ev) {
+    ev.preventDefault();
+    ev.stopPropagation();
+    console.log(ev.dataTransfer.files);
+    this.setState({
+      linkUrl: URL.createObjectURL(ev.dataTransfer.files[0])
+    });
+  }
+
+  dragOverFile(ev) {
+    ev.preventDefault();
+    ev.stopPropagation();
   }
 
   render() {
+    const { linkUrl } = this.state;
     return (
-      <label className="upload-wrapper">
-        <p>点击上传，或将文件拖拽到此处</p>
-        <input type="file" className="input-file" multiple />
+      <label className="upload-wrapper" onDrop={ev => this.dropFile(ev)} onDragOver={ev => this.dragOverFile(ev)}>
+        <input type="file" className="input-file" ref={this.fileInput} />
+        {linkUrl ? <img src={linkUrl} alt="" className="preview-img" /> : <p>点击上传，或将文件拖拽到此处</p>}
       </label>
     );
   }
